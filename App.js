0924e341c53ff173b2f6f9634213658a8900ad56
Swapna Soprala <swapna.soprala@analog.com>
2024-01-26 22:29:27 +0530

    JSX introduction

--- App.js
+++ App.js
@@ -2,27 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 /**
- * First parameter is the html tag
- * second parameter is the attributes to that tag
- * heading variable returns JS object in which "Hello world from react js!!!" is the child and {id : "heading", abc:"xyz"}, are the PROPS, console.log(heading)
- * render converts this object to html tag and put into root element
- * 
+ * React way of writing code to create `h1` element
+ * The code looks ugly right
+ * and also its very difficult to create h1 tag compared to HTML h1 tag creation
+ * Hence the concept of JSX comes into picture
+ * JSX is JS extension for creating react element
+ * JSX is diff and React is diff both are not same
  */
 
-/***
- * Nested HTML tag
- * You can create nested HTML tags by using createElement but the code looks ugly, and its very confusing
- *  Hence the JSX comes into picture
+/**
+ * Core React way of creating h1 element
  */
-const heading = React.createElement( "div", {id : "parent"}, [
-    React.createElement("h1", {id:"heading"},"I'm h1 tag"),
-    React.createElement("h2", {id:"heading"},"I'm h2 tag")],
-    React.createElement( "div", {id : "parent"}, [
-    React.createElement("h1", {id:"heading"},"I'm h1 tag"),
-    React.createElement("h2", {id:"heading"},"I'm h2 tag")]
-    ));
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "React - Laying the foundation 🛴"
+);
+console.log("heading ---->", heading);
+/**
+ * JSX way of creating h1 element
+ * JSX is transpiled before it reaches the JS engine
+ * And Parcel does that for you
+ * Parcel is like a manager so babel does this
+ */
+const jsxHeading = (
+  <h1 className="head" tabIndex={2}>
+    {" "}
+    React JSX h1 element (●'◡'●)
+  </h1>
+);
+console.log("jsxHeading", jsxHeading);
 
-    
-console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(jsxHeading);
