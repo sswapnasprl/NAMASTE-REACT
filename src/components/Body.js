c54652db24bbf60f4f6958ec52af8d8596c7f2e5
Swapna Soprala <swapna.soprala@analog.com>
2024-02-18 20:12:31 +0530

    Episode-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,53 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [ListofRestaurants, setListofRestaurants] = useState([]);
+  const [searchData, setSearchData] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
-  const [ListofRestaurants, setListofRestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.961144128793562&lng=77.70214509218931&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json_data = await data.json();
+      setListofRestaurants(
+        json_data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      );
+      setFilteredData(json_data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  console.log("body ");
 
   let listofRestaurants1 = [
-     {
-        id: "334475",
-        name: "KFC",
-        cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-        costForTwo: "400",
-        sla:{
-          deliveryTime: 36
-        },
-        avgRating: "3.8",
+    {
+      id: "334475",
+      name: "KFC",
+      cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      costForTwo: "400",
+      sla: {
+        deliveryTime: 36,
+      },
+      avgRating: "3.8",
     },
     {
-        id: "334476",
-        name: "MCD",
-        cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-        cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
-        costForTwo: "400",
-        sla:{
-          deliveryTime: 36
-        },
-        avgRating: "4.5",
+      id: "334476",
+      name: "MCD",
+      cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      costForTwo: "400",
+      sla: {
+        deliveryTime: 36,
+      },
+      avgRating: "4.5",
     },
     {
       id: "334477",
@@ -35,35 +55,63 @@ const Body = () => {
       cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
       cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
       costForTwo: "400",
-      sla:{
-        deliveryTime: 36
+      sla: {
+        deliveryTime: 36,
       },
       avgRating: "4.1",
     },
-
   ];
 
-    return (
-      <div className="body">
-        <div className="filter">
-        <button className="filter-btn" onClick={() => {
-          
-          const filteredList = ListofRestaurants.filter((res) => res.info.avgRating > 4.3
-            );           
-          console.log(filteredList);
-          setListofRestaurants(filteredList);
-        }}
-        >
-        Top Rated Restaurants</button>
-        </div>
-        <div className="res-container">
-          {resList.map((restaurant)=>(
-            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
-            //<RestaurantCard key={restaurant.id} resData={restaurant} />
-          ))}
-        </div>
-      </div>
-    );
-  };
+  // Conditional Rendering
+  // if(ListofRestaurants.length === 0){
+  //   return <Shimmer/>
+  // }
 
-  export default Body;
+  return ListofRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <input
+          type="text"
+          className="serach_box"
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
+        />
+
+        <button
+          onClick={() => {
+
+            const filteredData = ListofRestaurants.filter((res) => {
+              res.info.name.toLowerCase().includes(searchData.toLowerCase());              
+            });
+            setFilteredData(filteredData);
+          }}
+        >
+          Search
+        </button>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ListofRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            console.log(filteredList);
+            setListofRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredData.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          //<RestaurantCard key={restaurant.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
